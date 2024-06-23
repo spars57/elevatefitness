8b0a219e3d2b82f383c698b8a0a47c854f8f6d3e
Bruno Moisão <brunomoisao03@gmail.com>
2024-06-23 13:52:41 +0100

    update questions

--- src/components/faqs/mapper.ts
+++ src/components/faqs/mapper.ts
@@ -10,31 +10,19 @@ export const questionsMapper: QuestionMapper = [
   {
     question: "What do I need to get started?",
     response:
-      "After you finish the payment, we will contact you and ask you a few questions that we will need in order to create your workout plan.",
+      "All you need to do is proceed the payment, fill in the information needed in order to create your personalized program and we'll contact you ourselves on WhatsApp for further information and communication.",
     timeout: order(1),
   },
   {
     question: "How does this service work?",
     response:
-      "After we finish your workout plan, we will send you a document which it will provide everything you need to do in order for you to achieve the results you want, and also from that day on you can communicate 1 on 1 with your personal trainer by calls, messages and also ask him about various questions you may have until the 10 day trail it's over.",
+      "After we finish your workout plan, we will send you a document which it will provide everything you need to do in order for you to achieve the results you want, from training to diet and also from that day on you can communicate 1 on 1 with your personal trainer by calls, messages and also ask him about various questions you may have until the trail it's over.",
     timeout: order(2),
   },
   {
     question: "Where can I see real customer's results?",
     response:
-      "You can check our social media pages and check real customer results and reviews there",
+      "You can check the customers opinions in the upper section and you can contact us to send you some of the best transformations that our costumers have successfully achieved through the coaching program which is reliable every step of the way.",
     timeout: order(3),
-  },
-  {
-    question: "How do I cancel my subscription?",
-    response:
-      "As soon as you stop paying your subscription will expire, if you don't like the service you can ask for a refund within the first 3 days of subscribing.",
-    timeout: order(4),
-  },
-  {
-    question: "What if I don't like the service?",
-    response:
-      "If our service doesn't meet your expectations you can request a refund within the first 3 days of subscribing.",
-    timeout: order(5),
   },
 ];
