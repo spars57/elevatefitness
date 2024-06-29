122e41c065919a9ef6668f67debaff464a5a1d9e
Bruno Moisão <brunomoisao03@gmail.com>
2024-06-29 18:04:18 +0100

    fix gaps

--- src/components/pricing/card.tsx
+++ src/components/pricing/card.tsx
@@ -39,7 +39,7 @@ const PricingCard: FC<Props> = ({
     <StyledBox
       display="flex"
       flexDirection={"column"}
-      height={isMobileScreen() ? "auto" : 860}
+      height={isMobileScreen() ? "auto" : 1000}
       sx={{
         border: `1px solid ${theme.palette.grey[300]}`,
         borderRadius: 1,
@@ -57,7 +57,7 @@ const PricingCard: FC<Props> = ({
         display="flex"
         justifyContent={"center"}
         p={3}
-        height={isMobileScreen() ? "auto" : 450}
+        height={isMobileScreen() ? "auto" : 600}
       >
         <Typography variant="body1">{description}</Typography>
       </Box>
