0e62149ff61e54004cb8fbd988b51eb3a92791ce
Bruno Moisão <brunomoisao03@gmail.com>
2024-06-29 16:37:34 +0100

    add behavior to join us

--- src/components/quote/index.tsx
+++ src/components/quote/index.tsx
@@ -2,7 +2,6 @@ import { Box, Button, Fade, Slide, Typography, useTheme } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 
 import { isMobileScreen } from "../../constants";
-import scrollElementByIdIntoView from "../../utils/scroll-into-view";
 
 type Props = {
   src: string;
@@ -72,12 +71,13 @@ const Quote: FC<Props> = ({ src, quote }) => {
       </Fade>
       <Fade in={trigger} timeout={2000}>
         <Box mt={2} display="flex" width={"100%"} justifyContent={"center"}>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={scrollElementByIdIntoView("pricing")}
-          >
-            Join US
+          <Button variant="contained" color="secondary">
+            <a
+              style={{ textDecoration: "none", color: "white" }}
+              href="mailto:tomas.frasheri@gmail.com?subject=I%20want%20to%20join%20the%20group!&body=Before%20sending%20the%20email%20please%20answer%20this%20questions%3A%0D%0A%0D%0AWhat%20is%20your%20name%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20your%20nationality%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20your%20phone%20number%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20your%20age%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20your%20weight%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20your%20height%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20is%20the%20health%20situation%20that%20you%20are%20currently%20in%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AWhat%20types%20of%20goals%20do%20you%20want%20to%20achieve%20by%20pursuing%20this%20coaching%20program%3F%0D%0A%0D%0AR%3A%0D%0A%0D%0AThank%20you%20for%20reaching%20us%2C%20after%20sending%20this%20email%20a%20team%20member%20will%20contact%20you!"
+            >
+              Join US
+            </a>
           </Button>
         </Box>
       </Fade>
