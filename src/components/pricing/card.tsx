c27e60d7979b0695e31921c1d1df1c7dff8b1284
Bruno Moisão <brunomoisao03@gmail.com>
2024-06-29 16:27:48 +0100

    fix build

--- src/components/pricing/card.tsx
+++ src/components/pricing/card.tsx
@@ -32,7 +32,6 @@ const PricingCard: FC<Props> = ({
   description = " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   benefits,
   buttonColor = "primary",
-  onGetStarted = () => null,
 }) => {
   const theme = useTheme();
   return (
